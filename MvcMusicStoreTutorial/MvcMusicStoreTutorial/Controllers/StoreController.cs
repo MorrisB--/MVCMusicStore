1a0d51c421f8fc83c577ea0db8c38826cea45c55
MorrisB-- <morris.ballenger@yahoo.com>
2016-10-11 18:36:43 -0500

    Added main project

---
+++ MvcMusicStoreTutorial/MvcMusicStoreTutorial/Controllers/StoreController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStoreTutorial.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public String Index()
+        {
+            return "Hello from Store.Index()";
+        }
+
+        // GET: /Store/Browse?genre=Dicso
+        public String Browse(string genre)
+        {
+            String message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+            return message; 
+        }
+
+        // GET: /Store/Details/5
+        public String Details(int id)
+        {
+            String message = "Store.Details, ID = " + id;
+            return message; 
+        }
+    }
+}
---
+++ MvcMusicStoreTutorial/MvcMusicStoreTutorial/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcMusicStoreTutorial.MvcApplication" Language="C#" %>
