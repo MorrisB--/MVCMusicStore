ee7525ed2c28a20b3e1f1bc228d66a44bc4a082b
MorrisB-- <morris.ballenger@yahoo.com>
2016-11-01 16:48:18 -0500

    Added Zaniab's changes and fixed bugs

---
+++ Zaniab/MVCMusicStore_Team8/MVCMusicStore_Team8/Models/MusicStoreEntities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVCMusicStore_Team8.Models {
+    public class MusicStoreEntities : DbContext {
+        public MusicStoreEntities() : base("name=MusicStoreEntities") { }
+        public DbSet<MVCMusicStore_Team8.Models.Album> Albums { get; set; }
+        public DbSet<MVCMusicStore_Team8.Models.Genre> Genres { get; set; }
+        public DbSet<MVCMusicStore_Team8.Models.Artist> Artists { get; set; }
+    }
+}
